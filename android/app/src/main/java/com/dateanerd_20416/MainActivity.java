e884364349b46df270ec2da1d530b8c382ee658c
Crowdbotics <team@crowdbotics.com>
2020-09-18 21:01:09 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/dateanerd_20416/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dateanerd_20416;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "dateanerd_20416";
+    }
+}
